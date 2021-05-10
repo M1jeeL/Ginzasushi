d05b42d20f0118e9fda5da62af82af822dbdee4c
Miguel Loza <miguelloza1@hotmail.com>
2021-05-10 00:53:55 -0400

    Formulario registro finalizado, falta estilizarlo

--- src/components/FormRegistro/FormRegister.jsx
+++ src/components/FormRegistro/FormRegister.jsx
@@ -1,25 +1,143 @@
-import React from 'react'
-import InfoBox from './InfoBox'
+import React, { useState } from 'react'
 
 export default function FormRegister() {
+    const [form, setForm] = useState({});
+
+    const handleChange = e => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        alert("El formulario se ha enviado correctamente")
+    };
+
+    
+
     return (
         <>
-            <form className = "form-container">
-                <h1>Perfil</h1>
+            <form className = "form-container" onSubmit = {handleSubmit}>
+                <p>Perfil</p>
                 <hr />
-                <InfoBox info = 'Nombre' type= 'text'/>
-                <InfoBox info = 'Apellido' type= 'text'/>
-                <InfoBox info = 'Correo' type= 'email'/>
-                <InfoBox info = 'Confirmaci&oacute;n de correo' type= 'email'/>
-                <InfoBox info = 'Contrase&ntilde;a' type= 'password'/>
-                <InfoBox info = 'Confirmaci&oacute;n de contrase&ntilde;a' type= 'password'/> 
-                <InfoBox info = 'Tel&eacute;fono' type= 'tel'/> 
-                <h1>Direcci&oacute;n de despacho</h1>
+                <div>
+                    <label htmlFor="nombre">Nombre</label>
+                    <input 
+                    type="text" 
+                    id = "nombre" 
+                    name = "nombre" 
+                    value = {form.nombre}
+                    required
+                    onChange = {handleChange}
+                    />
+                    <br />
+                    <label htmlFor="apellido">Apellido</label>
+                    <input 
+                    type="text" 
+                    id = "apellido" 
+                    name = "apellido" 
+                    value = {form.apellido}
+                    required
+                    onChange = {handleChange}
+                    />
+                    <br />
+                    <label htmlFor="email">Correo</label>
+                    <input 
+                    type="email" 
+                    id = "email" 
+                    name = "email" 
+                    value = {form.email}
+                    required
+                    onChange = {handleChange}
+                    />
+                    <br />
+                    <label htmlFor="confirmacion_email">Confirmaci&oacute;n de correo</label>
+                    <input 
+                    type="email" 
+                    id = "confirmacion_email" 
+                    name = "confirmacion_email" 
+                    value = {form.confirmacionEmail}
+                    required
+                    onChange = {handleChange}
+                    />
+                    <br />
+                    <label htmlFor="nombre">Contrase&ntilde;a</label>
+                    <input 
+                    type="password" 
+                    id = "password" 
+                    name = "password" 
+                    value = {form.password}
+                    required
+                    onChange = {handleChange}
+                    />
+                    <br />
+                    <label htmlFor="nombre">Confirmaci&oacute;n de contrase&ntilde;a</label>
+                    <input 
+                    type="password" 
+                    id = "confirmacion_password" 
+                    name = "confirmacion_password" 
+                    value = {form.confirmacionPassword}
+                    required
+                    onChange = {handleChange}
+                    />
+                    <br />
+                    <label htmlFor="nombre">Celular</label>
+                    <input 
+                    type="tel" 
+                    id = "celular" 
+                    name = "celular" 
+                    value = {form.celular}
+                    placeholder = ""
+                    pattern = "[0-9 +]+"
+                    required
+                    onChange = {handleChange}
+                    />
+                    <br />
+                </div>
+                <br />
+                <br />
+                <p>Direcci&oacute;n de despacho</p>
                 <hr />
-                <InfoBox info = 'Comuna' type= 'checkbox'/> 
-                <InfoBox info = 'Calle' type= 'text'/> 
-                <InfoBox info = 'N&uacute;mero' type= 'number'/> 
-                <InfoBox info = 'Depto' type= 'text'/> 
+                <div>
+                    <label htmlFor="comuna">Comuna</label>
+                    <select name="comuna" id="comuna" defaultValue = "" required onChange = {handleChange}>
+                        <option value="" disabled>Seleccione su comuna</option>
+                        <option value="el_bosque">El Bosque</option>
+                        <option value="la_cisterna">La Cisterna</option>
+                        <option value="la_pintana">La Pintana</option>
+                    </select>
+                    <br />
+                    <label htmlFor="calle">Calle</label>
+                    <input 
+                    type="text"
+                    id = "calle"
+                    name = "calle"
+                    value = {form.calle}
+                    required
+                    onChange = {handleChange}
+                    />
+                    <label htmlFor="numeracion">Numero</label>
+                    <input 
+                    type="number"
+                    id = "numeracion"
+                    name = "numeracion"
+                    value = {form.numeracion}
+                    required
+                    onChange = {handleChange}
+                    />
+                    <label htmlFor="calle">Depto</label>
+                    <input 
+                    type="text"
+                    id = "depto"
+                    name = "depto"
+                    value = {form.depto}
+                    onChange = {handleChange}
+                    />
+                </div>
+                <input type="submit" value = "Crear cuenta" />
+
             </form>
         </>
     )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import logo from '../../img/logo.png'
 
 export default function Navbar() {
     return (
