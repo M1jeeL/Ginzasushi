34d34a0d86f140c41c8e6b40ec8994eb88877d56
Bastian <bastianrojaspoblete@gmail.com>
2021-10-19 16:30:56 -0300

    Detalles del pedido agregado, incompleto...

--- src/components/PanelUsuario/Pedidos/PedidoInfo.jsx
+++ src/components/PanelUsuario/Pedidos/PedidoInfo.jsx
@@ -35,6 +35,106 @@ const PedidoInfo = () => {
       <div className="container pedidos-container">
         <PanelUsuario />
         <div className="table-container-pedidos">
+          <div className="superior">
+            <ul className="barraEstado">
+              {pedido.estado === "Pendiente" && (
+                <>
+                  <li className="activado">Pendiente</li>
+                  <li className="">En camino</li>
+                  <li className="">Recibido</li>
+                </>
+              )}
+
+              {pedido.estado === "En camino" && (
+                <>
+                  <li className="activado">Pendiente</li>
+                  <li className="activado">En camino</li>
+                  <li className="">Recibido</li>
+                </>
+              )}
+
+              {pedido.estado === "Recibida" && (
+                <>
+                  <li className="activado">Pendiente</li>
+                  <li className="activado">En camino</li>
+                  <li className="Activado">Recibido</li>
+                </>
+              )}
+            </ul>
+          </div>
+          <div className="medio">
+            <div className="boxProductos">
+              <h1>Productos facturados: </h1>
+
+              {!pedido.items
+                ? ""
+                : pedido.items.map(
+                    (pedido, index, array) =>
+                      pedido.description !== "Despacho" && (
+                        <>
+                          <div className="boxProducto">
+                            <div className="imgProducto">
+                              <div className="img"></div>
+                            </div>
+                            <div className="infoProducto">
+                              <div className="tituloProducto">
+                                <h2>{pedido.title}</h2>
+                              </div>
+                              <div>
+                                <h4 className="descripcion">
+                                  Envoltura: {pedido.envoltura}
+                                </h4>
+                              </div>
+                              <div>
+                                <h4 className="unidades">
+                                  Unidades: {pedido.quantity}
+                                </h4>
+                              </div>
+                              <div className="precio">
+                                <h4>Precio: ${pedido.unit_price}</h4>
+                              </div>
+                            </div>
+                          </div>
+                        </>
+                      )
+                  )}
+            </div>
+
+            
+              
+                <div className="boxPrincipal">
+                  <div className="boxSuperior">
+                    <div className="titulo">
+                      <h1>Detalles Factura</h1>
+                    </div>
+                    <h3>Nombre: </h3>
+                    <h4>{pedido.estado}</h4>
+                    <h3>Celular: </h3>
+                    <h4>+569 67256867</h4>
+                    <h3>Correo: </h3>
+                    <h4>mail@gmail.com</h4>
+                  </div>
+                  <div className="boxInferior">
+                    <div className="titulo">
+                      <h1>Detalles Envío</h1>
+                    </div>
+
+                    <h3>Recibe: </h3>
+                    <h4>Juan Alverto</h4>
+                    <h3>Dirección: </h3>
+                    <h4>Avenida Apoquindo 543</h4>
+                    <h3>Precio:</h3>
+                    <h4>$2.500</h4>
+                  </div>
+                </div>
+              
+
+              <div className="precioTotal">
+                <h1>Total: </h1>
+              </div>
+            
+          </div>
+
           <Table dark striped className="pedidos-table">
             <thead>
               <tr>
@@ -66,6 +166,7 @@ const PedidoInfo = () => {
             </tbody>
           </Table>
         </div>
+        <div className="pedidoinfo"></div>
       </div>
     </>
   );
