1f68bd1e7f3abddfb4859d3b1ff2378967a170a1
Blindeus <85217980+Blindeus@users.noreply.github.com>
2021-10-22 14:25:33 -0300

    Agregada la seccion de pedidos al dashboard administrador, falta paginacion, arreglar botones, agregar filtrado y modal.

--- src/components/DashboardPedidos/DashboardPedidos.jsx
+++ src/components/DashboardPedidos/DashboardPedidos.jsx
@@ -1,10 +1,86 @@
-import React from "react";
+import React ,{ useState, useEffect } from "react";
 import { DashboardNavbar } from "../DashboardNavbar/DashboardNavbar";
+import {Input } from "reactstrap";
+import "./DashboardPedidos.scss"
+import DashboardPedidosTable from "./DashboardPedidosTable";
+import BotonPedido from "../BotonProps/BotonPedido.jsx"
+import { useHistory } from "react-router-dom";
+import _ from "lodash";
+
+const url = process.env.REACT_APP_PEDIDOS_API;
 
 export const DashboardPedidos = () => {
+  const history = useHistory();
+  const [pedidos, setPedidos] = useState([]);
+  const [paginatedPedidos, setPaginatedPedidos] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageNumberLimit] = useState(5);
+  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
+  const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
+  const pageSize = 10;
+useEffect(() => {
+  const token = localStorage.getItem("token");
+
+  fetch(`${url}/pedidos`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((pedidos) => {
+      setPedidos(pedidos);
+      setPaginatedPedidos(_(pedidos).slice(0).take(pageSize).value());
+    })
+    .catch((error) => {
+      console.log(error);
+      localStorage.removeItem("token");
+      history.push("/login");
+    });
+   
+  return () => {
+    setPedidos([]);
+    setPaginatedPedidos({});
+  };
+}, [history]);
+   console.log(pedidos)
   return (
-    <div>
-      <DashboardNavbar />
+    <div className="dashboard">
+      <DashboardNavbar/>
+      <div className="dashboar-pedidos-container">
+        <div className="dashboard-pedidos-header">
+          <div className="dashboard-pedidos-title">
+            Recepcion de pedidos
+          </div>
+          <div className="dashboard-pedidos-search">
+            <Input type="text"/>
+            <div className="dashboard-pedidos-icon-search">
+              <i className="fas fa-search fa-2x"></i>
+            </div>
+          </div>
+          </div > 
+          <div className="dashboard-pedidos-body">
+            <DashboardPedidosTable
+            pedidos={pedidos}
+            currentPage={currentPage}
+            pageSize={pageSize}
+            paginatedPedidos={paginatedPedidos}
+            setCurrentPage={setCurrentPage}
+            setPaginatedPedidos={setPaginatedPedidos}
+            pageNumberLimit={pageNumberLimit}
+            maxPageNumberLimit={maxPageNumberLimit}
+            minPageNumberLimit={minPageNumberLimit}
+            setMaxPageNumberLimit={setMaxPageNumberLimit}
+            setMinPageNumberLimit={setMinPageNumberLimit}
+            />
+          </div>
+          <div className="dashboard-pedidos-buttons">
+            <BotonPedido info="Editar" icono="far fa-edit fa-2x" />
+            <BotonPedido info="Eliminar" icono="fas fa-times fa-2x"/> 
+          </div>
+      </div>
     </div>
   );
 };
+
