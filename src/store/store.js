4a9da95a0b3fdeae884774958b5988c6a9160c6b
Miguel Loza <miguelloza1@hotmail.com>
2021-10-22 14:04:13 -0300

    Bug arreglado en la realización de pedidos

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,7 @@ import thunk from "redux-thunk";
 import { authReducer } from "../reducers/authReducer";
 import { productsReducer } from "../reducers/productsReducer";
 import { uiReducer } from "../reducers/uiReducer";
-
+import { pedidosReducer } from "../reducers/pedidosReducer";
 const composeEnhancers =
   (typeof window !== "undefined" &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
@@ -13,6 +13,7 @@ const reducers = combineReducers({
   auth: authReducer,
   products: productsReducer,
   ui: uiReducer,
+  pedidos: pedidosReducer,
 });
 
 export const store = createStore(
