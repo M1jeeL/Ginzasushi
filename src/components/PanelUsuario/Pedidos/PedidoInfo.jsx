a9366c29d49f8a5c07c2800e7602d61889389edb
Bastian Rojas <Bastianrojaspoblete@gmail.com>
2021-10-28 08:41:30 -0300

    arreglos v2

--- src/components/PanelUsuario/Pedidos/PedidoInfo.jsx
+++ src/components/PanelUsuario/Pedidos/PedidoInfo.jsx
@@ -2,15 +2,52 @@ import React, { useState, useEffect } from "react";
 import PanelUsuario from "../PanelUsuario";
 import Imgcab from "../../Imagen cabecera/Imgcab";
 import { useParams } from "react-router-dom";
-
 import { Button } from "reactstrap";
 import moment from "moment";
 import Loader from "../../Loader/Loader";
+import Swal from "sweetalert2";
+import { useSelector } from "react-redux";
 
 const PedidoInfo = () => {
   const { uuid } = useParams();
+  const { products } = useSelector((state) => state.products);
   const [pedido, setPedido] = useState({});
   const url = process.env.REACT_APP_PEDIDOS_API;
+
+  console.log(products);
+  const startRepeatOrder = () => {
+    /* let noHayStock = false;
+    pedido.items.forEach((element) => {
+      const [produ] = products.filter((product) => (product.id = element.id));
+      
+      if(produ?.activo === false){
+        noHayStock = true;
+      }
+      console.log(noHayStock);
+    });
+      if (noHayStock === true) {
+        Swal.fire({
+          title:
+            "Lamentablemente uno de los productos no se encuentra disponible",
+          text: "Vuelve a realizar un nuevo pedido",
+          icon: "error",
+        });
+      } else { */
+    Swal.fire({
+      title: "¿Seguro que deseas realizar nuevamente éste pedido?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Confirmar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Confirmado");
+      }
+    });
+    //}
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -64,7 +101,7 @@ const PedidoInfo = () => {
             </ul>
 
             <div className="pedido-productos">
-              <h1>Productos facturados: </h1>
+              <h1>Productos facturados:</h1>
               {!pedido.items
                 ? ""
                 : pedido.items.map(
@@ -72,14 +109,20 @@ const PedidoInfo = () => {
                       pedido.description !== "Despacho" && (
                         <div key={index} className="pedido-productos-detalle">
                           <div className="pedido-productos-detalle-bodyImage">
-                            <div className="pedido-productos-detalle-bodyImage-image"></div>
+                            <div>
+                              <img
+                                src="https://picsum.photos/500"
+                                alt="sushi"
+                                className="pedido-productos-detalle-bodyImage-image"
+                              />
+                            </div>
                           </div>
                           <div className="pedido-productos-detalle-bodyTitle">
                             <div className="pedido-productos-detalle-bodyTitle-title">
                               <h2>{pedido.title}</h2>
                             </div>
                             <div className="pedido-productos-detalle-bodyEnvolture-envolture">
-                              <h3>Envoltura:</h3>
+                              <h3>Envoltura: </h3>
                               <h4>{pedido.envoltura}</h4>
                             </div>
                             <div>
@@ -141,7 +184,13 @@ const PedidoInfo = () => {
 
             <div className="pedido-repetir">
               <h1>¿Te gustaría solicitar nuevamente éste pedido?</h1>
-              <Button color="success">Repetir Pedido</Button>
+              <Button
+                color="warning"
+                outline
+                onClick={() => startRepeatOrder()}
+              >
+                Repetir Pedido
+              </Button>
             </div>
           </div>
         ) : (
