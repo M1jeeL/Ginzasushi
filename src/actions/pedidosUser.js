e6f135f94bd66534bd5933667472b3fabdd6d2f3
Bastian Rojas <Bastianrojaspoblete@gmail.com>
2021-11-28 23:37:29 -0300

    Especificación de productos no disponibles mostrados en alerta (COMPLETADO)

--- src/actions/pedidosUser.js
+++ src/actions/pedidosUser.js
@@ -1,3 +1,4 @@
+import { clamp } from "lodash";
 import Swal from "sweetalert2";
 import { loadPedidosUser } from "../helpers/loadPedidosUser";
 import { types } from "../types/types";
@@ -67,7 +68,7 @@ export const startRepeatOrder = (pedido) => {
     let noHayStock = false;
     const { products } = getState().products;
     const titleProduct = []; //Almacena los nombres de los productos no disponibles
-    const mensaje = ""
+    let mensaje = "";
 
     pedido.items.forEach((element) => {
       // eslint-disable-next-line eqeqeq
@@ -79,12 +80,24 @@ export const startRepeatOrder = (pedido) => {
       }
     });
 
-    
+    if (titleProduct.length === 2) {
+      mensaje = "Los siguientes productos no se encuentran disponibles: " + titleProduct.join(" y ");
+      console.log(mensaje);
+    }
+    else if(titleProduct.length === 1){
+        mensaje = "El siguiente producto no se encuentra disponible: " + titleProduct.join("");
+    }
+    else{
+        for(let i = 0; i < (titleProduct.length)-1; i++){
+            mensaje = mensaje + titleProduct[i] + ", ";
+        }
+        mensaje = "Los siguientes productos no se encuentran disponibles: "+ mensaje.substring(0 , mensaje.length - 2) + " y " + titleProduct[(titleProduct.length - 1)];
+    }
+
     if (noHayStock === true) {
       Swal.fire({
-        title:
-          "Lamentablemente ahora no podrás repetir éste pedido",
-        text: "Los siguientes productos no se encuentran disponibles: " + titleProduct.join(', '),
+        title: "Lamentablemente ahora no podrás repetir éste pedido",
+        text: mensaje,
         icon: "error",
       });
     } else {
